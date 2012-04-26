773e50606eb61886309ff9f102eb28d72f797247
Sergio Lopes <slopes@gmail.com>
2012-04-26 19:22:52 -0300

    porting vraptor3 converters package

---
+++ vraptor-cdi-core/src/org/vraptor/converter/jodatime/LocalDateConverter.java
@@ -0,0 +1,55 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+
+package org.vraptor.converter.jodatime;
+
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import org.joda.time.LocalDate;
+import org.vraptor.converter.ConversionError;
+import org.vraptor.converter.Convert;
+import org.vraptor.converter.Converter;
+import org.vraptor.core.Localization;
+
+/**
+ * VRaptor converter for {@link LocalDate}. {@link LocalDate} is part of Joda Time library.
+ * 
+ * @author Lucas Cavalcanti
+ */
+@Convert(LocalDate.class)
+public class LocalDateConverter implements Converter<LocalDate> {
+
+    private final Localization localization;
+
+	public LocalDateConverter(Localization localization) {
+        this.localization = localization;
+    }
+
+    public LocalDate convert(String value, Class<? extends LocalDate> type, ResourceBundle bundle) {
+		try {
+			Date date = new LocaleBasedJodaTimeConverter(localization).convert(value, type);
+			if (date == null) {
+				return null;
+			}
+			return LocalDate.fromDateFields(date);
+		} catch (Exception e) {
+			throw new ConversionError(MessageFormat.format(bundle.getString("is_not_a_valid_date"), value));
+		}
+	}
+}
---
+++ vraptor-cdi-core/src/org/vraptor/converter/jodatime/LocaleBasedJodaTimeConverter.java
@@ -0,0 +1,63 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.vraptor.converter.jodatime;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
+
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.base.BaseLocal;
+import org.vraptor.core.Localization;
+
+class LocaleBasedJodaTimeConverter {
+
+    private final Localization localization;
+
+	public LocaleBasedJodaTimeConverter(Localization localization) {
+		this.localization = localization;
+	}
+
+	public Date convert(String value, Class<? extends BaseLocal> type) throws ParseException {
+	    if (isNullOrEmpty(value)) {
+			return null;
+		}
+	    
+		return getDateFormat(type).parse(value);
+	}
+
+	public Locale getLocale() {
+		Locale locale = localization.getLocale();
+		if (locale == null) {
+			locale = Locale.getDefault();
+		}
+		return locale;
+	}
+
+	private DateFormat getDateFormat(Class<? extends BaseLocal> type) {
+		if (type.equals(LocalTime.class)) {
+			return DateFormat.getTimeInstance(DateFormat.SHORT, getLocale());
+		} else if (type.equals(LocalDateTime.class)) {
+			return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, getLocale());
+		}
+		
+		return DateFormat.getDateInstance(DateFormat.SHORT, getLocale());
+	}
+}
